c9702972d49d2aa4c2f85dbb945840b7d5e18913
Latif080790 <latiefprasetya1211@gmail.com>
2026-02-28 03:31:44 +0800

    test: wcag pre-commit hook (should succeed)

---
+++ test-wcag-violation.tsx
@@ -0,0 +1,6 @@
+// Test file for WCAG pre-commit hook validation
+// This should PASS - compliant with WCAG
+
+export const testComponent = () => {
+  return <div className="text-xs">This is WCAG compliant</div>;
+};
