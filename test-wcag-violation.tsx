f69bea65091d86dac9bd48b6697c8409df828ad5
Latif080790 <latiefprasetya1211@gmail.com>
2026-02-28 03:31:57 +0800

    chore: remove test file

--- test-wcag-violation.tsx
+++
@@ -1,6 +0,0 @@
-// Test file for WCAG pre-commit hook validation
-// This should PASS - compliant with WCAG
-
-export const testComponent = () => {
-  return <div className="text-xs">This is WCAG compliant</div>;
-};
